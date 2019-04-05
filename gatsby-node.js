1e8308007100f221cc99b98b5586e7d2f5fd0391
Viren Bhagat <virenb@gmail.com>
2019-04-05 14:58:46 -0400

    Update createPages

---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/pages/blog-post.js`)
+  return graphql(`
+    {
+      allContentfulBlog(limit: 100) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    result.data.allContentfulBlog.edges.forEach(edge => {
+      createPage({
+        path: `${edge.node.fields.slug}`,
+        component: blogPostTemplate,
+        context: {
+          slug: edge.node.slug,
+        },
+      })
+    })
+  })
+}
