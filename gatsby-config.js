0f1f12b0b5e3744c52c3604baf2994be65a80b44
Viren Bhagat <virenb@gmail.com>
2019-07-26 16:35:18 -0400

    Using default starter..

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,34 @@
-require("dotenv").config()
-
 module.exports = {
   siteMetadata: {
-    title: "Viren Bhagat",
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    // {
-    // 	resolve: 'gatsby-source-filesystem',
-    // 	options: {
-    // 		name: 'images',
-    // 		path: `${__dirname}/src/images`
-    // 	}
-    // },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-remark",
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/projects`,
-        name: "projects",
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: "gatsby-source-contentful",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        spaceId: process.env.spaceId,
-        accessToken: process.env.accessToken,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
